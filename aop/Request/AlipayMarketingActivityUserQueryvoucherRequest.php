5f488f9cb03436cc99cdc055b5fa11c0e143c068
wyq <407069461@qq.com>
2022-05-05 14:22:33 +0800

    add 查询活动详情,条件查询用户券,查询用户券详情

---
+++ aop/Request/AlipayMarketingActivityUserQueryvoucherRequest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * ALIPAY API: alipay.marketing.activity.user.queryvoucher
+ *
+ * @author auto create
+ *
+ * @since  1.0, 2017-04-07 18:22:19
+ */
+
+namespace Alipay\Request;
+
+class AlipayMarketingActivityUserQueryvoucherRequest extends AbstractAlipayRequest
+{
+    /**
+     * 商家营销活动咨询接口
+     **/
+    private $bizContent;
+
+    public function setBizContent($bizContent)
+    {
+        $this->bizContent = $bizContent;
+        $this->apiParams['biz_content'] = $bizContent;
+    }
+
+    public function getBizContent()
+    {
+        return $this->bizContent;
+    }
+}
